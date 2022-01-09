44e8f1ff9f022c38208daa5667c7b721e9b87738
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-01-09 03:34:16 +0300

    create a profile class

--- majiraniapp/models.py
+++ majiraniapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from cloudinary.models import CloudinaryField
+from django.utils import timezone
+from django.contrib.contenttypes.fields import GenericRelation
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile',null=True)
+    bio = models.TextField(max_length=200)
+    email = models.EmailField()
+    profile_picture = CloudinaryField('image')
+
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
