11c104efe47f56398dd9eafbefea6f45c3412a2d
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-01-09 13:40:15 +0300

    create a neigborhood form

--- majiraniapp/forms.py
+++ majiraniapp/forms.py
@@ -6,3 +6,8 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ("bio","location", "profile_picture")
+
+class NeigborhoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighborhood
+        fields = ("neigname","neigcount", "neiglocation")
